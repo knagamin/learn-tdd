8a0516b097b0176f8844b3b7070e875fa13712f0
Kiyo Nagamine <knagamin.hub@gmail.com>
2019-10-16 12:48:19 +0900

    initial commit

---
+++ money/tests/test_money.py
@@ -0,0 +1,20 @@
+"""
+$5 + 10 CHF = $10 (レートが2:1の場合)
+[DONE] $5 * 2 = $10
+amountをprivateにする
+Dollarの副作用をどうする？
+Moneyの丸め処理どうする？
+"""
+
+import unittest
+from money import Dollar
+
+
+class TestMoney(unittest.TestCase):
+
+    def test_maltiplication(self):
+        five = Dollar(5)
+        five.times(2)
+        self.assertEqual(10, five.amount)
+        five.times(3)
+        self.assertEqual(15, five.amount)
