58b2792ecd530c57a77e0eb987c66775aa472e2f
Kiyo Nagamine <knagamin.hub@gmail.com>
2019-10-23 10:15:33 +0900

    changed: implemented currency method

--- money/money.py
+++ money/money.py
@@ -1,34 +1,55 @@
-class Money:
+from abc import ABCMeta, abstractmethod
+
+
+class Money(metaclass=ABCMeta):
     _amount = 0
+
+    def __init__(self, amount, currency):
+        self._amount = amount
+        self._currency = currency
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self._amount == other._amount
-
         return False
+
+    def currency(self):
+        return self._currency
+
+    @abstractmethod
+    def times(self):
+        pass
 
     @staticmethod
     def dollar(amount):
-        return Dollar(amount)
+        return Dollar(amount, "USD")
 
     @staticmethod
     def franc(amount):
-        return Franc(amount)
+        return Franc(amount, "CHF")
 
 
 class Dollar(Money):
 
-    def __init__(self, amount):
+    def __init__(self, amount, currency):
         self._amount = amount
+        self._currency = "USD"
+
+    def currency(self):
+        return self._currency
 
     def times(self, multiplier):
-        return Dollar(self._amount * multiplier)
+        return Money.dollar(self._amount * multiplier)
 
 
 class Franc(Money):
 
-    def __init__(self, amount):
+    def __init__(self, amount, currency):
         self._amount = amount
+        self._currency = "CHF"
+
+    def currency(self):
+        return self._currency
 
     def times(self, multiplier):
-        return Franc(self._amount * multiplier)
+        return Money.franc(self._amount * multiplier)
